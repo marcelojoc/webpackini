eb2a551e2eb04eec9bf61dbbf5856c3f40858846
Marcelo Contreras <marcelojoc@gmail.com>
2018-06-13 00:37:48 -0300

    uso de url-loader  para utilizar imagenes

--- url-loader-images/index.js
+++ url-loader-images/index.js
@@ -1,7 +1,7 @@
-import './estilos.css';              // importo estilos
+import './estilos.css';
 import message from './message.js';
 import element from './element.js';
-
+import platzi from './platzi.jpg'   // importo la imagen
 
 document.write(message.firstmessage);
 // message.save();
@@ -15,6 +15,9 @@ let midiv= element.createDiv();
 console.log(midiv);
 document.body.appendChild(midiv);
 
-
+const img = document.createElement('img');
+img.setAttribute('src', platzi);
+document.body.appendChild(img);
+ 
 
 console.log('estilos desde loader');
---
+++ url-loader-images/webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+
+module.exports={                // exporto el modulo de configuracion
+    entry: path.resolve(__dirname, 'index.js'),  // archivo de entrada
+    output:{
+        path: path.resolve(__dirname, 'dist'),   // archivo de salida  directorio de destino y nombre de compilacion al final
+        filename: 'bundle.js'
+    },
+
+    module:{    // are a de plugins y loaders
+
+        rules:[
+            
+            // aqui se colocan los loaders
+            {
+                // test:  que tipo de archivo quiero reconocer
+                // use:  que loader se va a encargar del archivo
+                test: /\.js$/,
+                //Loader to use
+                exclude: /(node_modules)/,
+                use: {
+                    loader:'babel-loader',
+                    options:{
+                        presets:['es2015']
+                    }
+
+                }
+                
+            },
+            {
+                // test:  que tipo de archivo quiero reconocer
+                // use:  que loader se va a encargar del archivo
+                test: /\.css$/,
+                //Loader to use
+                use: ExtractTextPlugin.extract({        
+                  fallback: 'style-loader',
+                  use: 'css-loader'
+                })
+                
+            },
+            {
+
+                test:/\.(jpg|png|gif)$/,
+                use:{
+                    loader:'url-loader',
+                    options:{
+                        limit: 100000,   // hasta que limite de
+
+                    }
+
+
+                }
+
+
+
+
+            }
+
+
+
+
+        ]
+    },
+
+    plugins: [
+        //Call plugins here
+        new ExtractTextPlugin("css/[name].css") //name permite uusar el nombre original del entrypoint
+      ]
+
+}
