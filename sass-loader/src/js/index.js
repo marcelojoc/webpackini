71b4f6edbc3bd5e0d4c6da2bd0f71774cacaf61b
Marcelo Contreras <marcelojoc@gmail.com>
2018-07-07 18:56:02 -0300

    pre procesador con sass  configuracion funcional

---
+++ sass-loader/src/js/index.js
@@ -0,0 +1,40 @@
+import '../css/estilos.css';
+import '../css/personas.scss';
+
+import message from './message.js';
+import element from './element.js';
+import renderToDOM from './render-to-dom.js';
+import platzi from '../img/platzi.jpg';   // importo la imagen
+import data from './personas.json';
+
+
+// importo react y react dom
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+
+console.log(data);
+document.write(message.firstmessage);
+
+
+
+
+let x;
+for (x in data) {
+    
+    let li= document.createElement('li');   
+    li.innerHTML=data[x].name;
+    li.setAttribute("class", "Persona");
+    renderToDOM(li);
+}
+
+
+let midiv= element.createDiv();
+renderToDOM(midiv);
+
+
+
+const img = document.createElement('img');
+img.setAttribute('src', platzi);
+document.body.appendChild(img);
+ 
