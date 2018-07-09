6568ec068c1840719ef49b1c3e338290f8d80d63
Marcelo Contreras <marcelojoc@gmail.com>
2018-07-08 23:25:02 -0300

    postcss loader en accion

---
+++ postcss-loader/src/js/index.js
@@ -0,0 +1,38 @@
+import '../css/estilos.css';
+// import '../css/personas.scss';
+ import '../css/personas.styl';
+// import '../css/personas.less';
+
+import message from './message.js';
+import element from './element.js';
+import renderToDOM from './render-to-dom.js';
+import platzi from '../img/platzi.jpg';   // importo la imagen
+import data from './personas.json';
+
+
+console.log(data);
+document.write(message.firstmessage);
+
+
+
+
+let x;
+for (x in data) {
+    
+    let li= document.createElement('li');   
+    li.innerHTML=data[x].name;
+    li.setAttribute("class", "Persona");
+    renderToDOM(li);
+}
+
+
+let midiv= element.createDiv();
+renderToDOM(midiv);
+// 
+
+
+const img = document.createElement('img');
+img.setAttribute('src', platzi);
+img.setAttribute("class", "logo");
+document.body.appendChild(img);
+ 
---
+++ postcss-loader/src/js/message.js
@@ -0,0 +1,12 @@
+module.exports= {
+
+    firstmessage:'Hola mundo desde un modulo ',
+
+    save: function (){
+
+        alert('alert desde un modulo');
+
+    },
+
+
+}
