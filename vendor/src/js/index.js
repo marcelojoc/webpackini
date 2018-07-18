1c9a642dd9f8e5ab9a4adcdf408c8f1617710c7b
Marcelo Contreras <marcelojoc@gmail.com>
2018-07-18 19:31:46 -0300

    dependencias comunes en un solo archivo

---
+++ vendor/src/js/index.js
@@ -0,0 +1,40 @@
+import '../css/estilos.css';
+// import '../css/personas.scss';
+ import '../css/personas.styl';
+// import '../css/personas.less';
+
+import message from './message.js';
+import element from './element.js';
+import renderToDOM from './render-to-dom.js';
+import platzi from '../img/platzi.jpg';   // importo la imagen
+import data from './personas.json';
+import React from 'React';
+import edom from 'react-dom';
+
+
+console.log(data);
+document.write(message.firstmessage);
+
+
+
+
+let x;
+for (x in data) {
+    
+    let li= document.createElement('li');   
+    li.innerHTML=data[x].name;
+    li.setAttribute("class", "Persona");
+    renderToDOM(li);
+}
+
+
+let midiv= element.createDiv();
+renderToDOM(midiv);
+// 
+
+
+const img = document.createElement('img');
+img.setAttribute('src', platzi);
+img.setAttribute("class", "logo");
+document.body.appendChild(img);
+ 
